a108908187704097d100e642d67d85266d5eb6b0
Sumaya Irfan <irfansum@sheridancollege.ca>
2025-05-07 14:34:24 -0400

    Add project files.

---
+++ W1ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace W1ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            //Console.Write("Enter circle radius: ");
+            //double r = double.Parse(Console.ReadLine());
+            //double diameter = 2 * r;
+            //double circumference = 2 * Math.PI * r;
+            //double area = Math.PI * r * r;
+            //Console.WriteLine("Diameter: " + diameter);
+            //Console.WriteLine("Circumference: " + circumference);
+            //Console.WriteLine("Area: " + area);
+
+            int[] numbers = { 10, 20, 0, 30 };
+            int total = 0;
+            int count = numbers.Length;
+
+            for (int i = 0; i < count; i++) // off-by-one error
+            {
+                total += numbers[i];
+            }
+
+            int average = total / count;
+            Console.WriteLine("Average: " + average);
+
+
+        }
+    }
+}
